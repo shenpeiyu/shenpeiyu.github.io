116c1fa3c1dd5192027686d390f36d00d1f47195
shenpeiyu <48717235+shenpeiyu@users.noreply.github.com>
2019-06-02 21:28:55 +0800

    Add files via upload

---
+++ hw2/js/index.js
@@ -0,0 +1,115 @@
+// 當文件已經全載入至記憶體時，開始執行程式
+$(document).ready(function() {
+
+        // 清空 product-list
+        $('#product-list').empty();
+        $('#page').hide()
+
+        var items = null
+        var pageCount = 20
+        var showItems = (page) => {
+            if (items == null) return
+            var start = (page - 1) * pageCount
+            var end = start + pageCount - 1
+            $('#product-list').empty();
+            for (var i = start; i <= end; i++) {
+                if (items[i] == null)
+                    break;
+                newItem(items[i])
+            }
+        }
+
+
+        var newItem = (item) => {
+            $img = $('<img>').attr('class', 'image').attr('src', item.image)
+            $h3 = $('<h3>').attr('class', 'name').text(item.name)
+            $p = $('<p>').attr('class', 'price').text('NT$ ' + item.price)
+
+            $item = $('<div>').attr('class', 'item').append($img)
+            $info = $('<div>').attr('class', 'info').append($h3).append($p)
+            $col = $('<div>').attr('class', 'col-*').append($item).append($info)
+
+            $('#product-list').append($col)
+        }
+
+        var newPage = (n) => {
+            var pageNum = n / 20
+            pageNum = (n % 20 != 0) ? pageNum + 1 : pageNum
+
+            $('#page-number').empty()
+
+            $la = $('<a>').attr('class', 'page-link').attr('href', '#').attr('tabindex', '-1').attr('aria-disabled', 'true').text('«')
+            $lli = $('<li>').attr('class', 'page-item').addClass('disabled').append($la)
+
+            $('#page-number').append($lli)
+
+            // 插入分頁數字
+            for (var i = 1; i <= pageNum; i++) {
+                $a = $('<a>').attr('class', 'page-link').attr('href', '#').text(i)
+
+                $a.on('click', function() {
+                    var i = $(this).text()
+                    showItems(Number(i))
+
+                    $('li.page-item').attr('class', 'page-item')
+                    $(this).parent().addClass('active')
+                })
+
+
+                var strActive = ((i == 1) ? ' active' : '')
+                $li = $('<li>').attr('class', 'page-item' + strActive).append($a)
+                $('#page-number').append($li)
+            }
+
+            $ra = $('<a>').attr('class', 'page-link').attr('href', '#').text('»')
+            $rli = $('<li>').attr('class', 'page-item').append($ra)
+            $('#page-number').append($rli)
+        }
+
+        $('#pills-query-tab').on('click', function() {
+            $.get('https://js.kchen.club/B07106012/query', function(response) {
+                if (response) {
+                    // 伺服器有回傳資料
+                    if (response.result) {
+                        $('#product-list').empty();
+                        // 資料庫有回傳資料
+                        items = response.items
+
+                        // for (var i = 0; i < items.length; i++) {
+                        //     newItem(items[i])
+                        // }
+
+                        // 加了分頁效果，預設顯示第一頁
+                        showItems(1)
+
+                        // 顯示分頁和設定分頁的函式
+                        $('#page').show()
+                        newPage(items.length)
+
+                    } else {
+                        $('#message').text('查無相關資料')
+                        $('#dialog').modal('show')
+                    }
+                } else {
+                    $('#message').text('伺服器出錯')
+                    $('#dialog').modal('show')
+                }
+
+
+                console.log(response)
+            }, "json")
+        })
+    })
+    // 置頂按鍵
+$(function() {
+    $('#BackTop').click(function() {
+        $('html,body').animate({ scrollTop: 0 }, 333);
+    });
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('#BackTop').fadeIn(222);
+        } else {
+            $('#BackTop').stop().fadeOut(222);
+        }
+    }).scroll();
+})
